4b35063faa43172100379f8596de4592cde288e6
Yeonuk Hwang <younuk23@gmail.com>
2022-10-19 02:06:33 +0900

    ADD: desinate excutable path in puppeteer
    desinate excutable path in puppeteer to local chrome because chromium is not bundled
    while packaging

--- src/main/scrapper/index.ts
+++ src/main/scrapper/index.ts
@@ -1,25 +1,17 @@
+import { app } from 'electron';
+import puppeteer from 'puppeteer';
 import { InsScarpperImpl } from './scrapper';
 import { ScrapperManager } from './scrapperManager';
 
 export async function makeScrapper() {
-  const PCR = require('puppeteer-chromium-resolver');
-  const option = {
-    revision: '',
-    detectionPath: '',
-    folderName: '.chromium-browser-snapshots',
-    defaultHosts: [
-      'https://storage.googleapis.com',
-      'https://npm.taobao.org/mirrors',
-    ],
-    hosts: [],
-    cacheRevisions: 2,
-    retry: 3,
-    silent: false,
+  const CHROME_PATHS: Partial<Record<typeof process.platform, string>> = {
+    darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    win32: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
   };
-  const stats = await PCR(option);
-  const browser = await stats.puppeteer.launch({
+
+  const browser = await puppeteer.launch({
     args: ['--disk-cache-size=0', '--lang=en-US', '--no-sandbox'],
-    executablePath: stats.executablePath,
+    executablePath: CHROME_PATHS[process.platform],
   });
 
   const scrapper = new InsScarpperImpl(browser);
